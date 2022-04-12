88c11065b1bc6b34777bde7e7617acce49e2ce74
Olga <charizmo4ka@icloud.com>
2022-04-12 17:50:39 +0300

    JS homework 15

---
+++ HW15/main.js
@@ -0,0 +1,105 @@
+
+
+//howework15 
+//Task1
+let x = 20
+let y = 58
+let z = 42
+let sum = x + y + z
+console.log(sum);
+
+//Task2
+
+let secInMin = 60
+let minInHour = 60
+let secInHour = secInMin * minInHour;
+console.log(secInHour);
+
+
+let hoursInDay = 24
+let secInDay = secInHour * hoursInDay;
+console.log(secInDay);
+
+let daysInYear = 365
+let secInYear = secInDay * daysInYear;
+console.log(secInYear);
+
+let myAgeInYears = 36
+let myAgeInSeconds = myAgeInYears * secInYear
+console.log(myAgeInSeconds);
+
+//!!!Task3 don't understand task
+
+let count = 42
+let userName = "42"
+console.log(userName + count);
+
+let count1 = count.toString()
+let userName1 = Number(userName)
+
+console.log("count " + typeof (count1))
+console.log("userName " + typeof (userName1))
+
+
+//Task4
+
+let a = "1"
+let b = "2 "
+let c = "белых медведей"
+console.log(a + b + c);
+
+//Task5
+let d = "доступ"
+console.log(d.length);
+let e = "морпех"
+console.log(e.length);
+let f = "наледь"
+console.log(f.length);
+let g = "попрек"
+console.log(g.length);
+let h = "рубило"
+console.log(h.length);
+let length = d + e + f + g + h;
+console.log(length.length);
+
+//Task6 dont understand what to do 
+
+let name = 3
+let type = "4"
+let variable
+console.log("Variable: " + typeof (name) + " have type: " + typeof (type));
+
+
+
+//Advanced 
+//Task1
+
+let uName = prompt("Enter User Name");
+let age = prompt("Enter your age");
+console.log("task1 user name:" + uName + " age:" + age);
+
+
+
+//Task2
+
+let codeWord1 = "обернись";
+
+let codeWord2 = "неужели";
+
+let codeWord3 = "огурцы";
+
+let codeWord4 = "липкие";
+
+let codeWord5 = "?!";
+
+let cipher = "codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]";
+console.log(cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
+
+
+alert("hello")
+
+let p = confirm("agree?")
+console.log(p);
+
+const magic = '\nyou'
+console.log(magic[1]);
